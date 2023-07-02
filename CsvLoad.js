96e3e88ca833d31a44376281f9dec4d64c2d4016
Surya10102000 <sfrost488@gmail.com>
2023-07-02 18:53:47 +0530

    fix minor bug

--- CsvLoad.js
+++ CsvLoad.js
@@ -1,32 +1,67 @@
+const app = require('express')();
 const fs = require('fs');
 const csvParser = require('csv-parser');
-const { DataTypes, col } = require('sequelize');
+const { DataTypes } = require('sequelize');
 const DynamicModel = require('./models/DynamicModel');
 const sequelize = require('./config/database');
 
 const csvFilePath = 'organizations-100000.csv';
-const tableName = 'TestPDCEncum'; // Replace with the desired table name
+const tableName = 'TestCsvTable'; // Replace with the desired table name
 
-fs.createReadStream(csvFilePath)
+ fs.createReadStream(csvFilePath)
   .pipe(csvParser({ separator: ',' }))
-  .on('headers', (headers) => {
-    const columnDefinitions = {};
+  .on('headers', async (headers) => {
+    try {
+      const columnDefinitions = {};
+      headers.forEach((header) => {
+        columnDefinitions[header] = {
+          type: DataTypes.TEXT,
+          allowNull: true, // Adjust as needed
+        };
+      });
+       DynamicModel.init(columnDefinitions, {
+        sequelize,
+        tableName,
+        //modelName: 'DynamicModel',
+      });
+    } catch (error) {
+      console.error('Error creating table:', error.message);
+    }
+  })
 
-    headers.forEach(header => {
-      columnDefinitions[header] = {
-        type: DataTypes.TEXT,
-        allowNull: true, // Adjust as needed
-      };
-    });
 
-    DynamicModel.init(columnDefinitions, {
-      sequelize,
-      tableName,
-      //modelName: 'DynamicModel',
-    })
+app.get('/:id', (req, res)=>{
 
-    sequelize.sync({ force: true });
+  DynamicModel.findByPk( req.params.id ).then(data=>{
+
+    res.status(200).json({ Users: data})
+  })
+  .catch(err=>{
+    res.status(501).json({ message: err.message})
 
   })
-  .on('data', (row) => DynamicModel.create(row))
-  .on('end', () => sequelize.close());
+})
+
+  sequelize.sync({ force: true }).then(result=>{
+    try {
+      app.listen(3000, console.log(`Server up at 3000`));
+      fs.createReadStream(csvFilePath)
+        .pipe(csvParser({ separator: ',' }))
+        .on('data', async (row) => {
+          try {
+            await DynamicModel.create(row);
+          } catch (error) {
+            console.error('Error inserting row:', error);
+          }
+        })
+  
+  
+    } catch (err) {
+      console.log("Error occured ", err.message);
+    }
+  })
+  .catch(err=>{
+    console.log(`Server errror`, err.message);
+    process.exit(1);
+  })
+  
