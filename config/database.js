6815d6663a134a5e754e325da5431471d265b10f
sunilkr09 <sk2321996@gmail.com>
2023-07-03 11:52:42 +0530

    Disabled Logging to Console

--- config/database.js
+++ config/database.js
@@ -2,7 +2,8 @@ const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize({
   dialect: 'sqlite',
-  storage: 'db.sqlite' // Replace with the actual path to your SQLite database file
+  storage: 'db.sqlite', // Replace with the actual path to your SQLite database file
+  logging: false
 });
 
 module.exports = sequelize;
